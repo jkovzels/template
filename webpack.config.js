d8cf6dbbbd4c3296e7c1d19a3b6a6a74c70bcac1
jk <jurijs.kovzels@outlook.com>
2017-01-22 16:28:50 +0100

    adding scss procesing

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@
 // We need this plugin to detect a `--watch` mode. It may be removed later
 // after https://github.com/webpack/webpack/issues/3460 will be resolved.
 const { CheckerPlugin } = require('awesome-typescript-loader');
+
 const { resolve } = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const sassLoaders = [
+	'css-loader?sourceMap',
+	'sass-loader?includePaths[]=' + resolve(__dirname, './src/styles')
+];
+
 module.exports = {
 	entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        './src/index.ts',
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/only-dev-server',
+		'./src/index',
 	]
 	, output: {
 		filename: 'bundle.js'
@@ -18,21 +24,20 @@ module.exports = {
 		, publicPath: '/public/'
 	}
 	, devServer: {
-    devServer: {
-		publicPath: '/public/',
-        colors: true,
-        hot: true,
-        inline: true,
-        progress: true,
-        historyApiFallback: true,
-    },
+		devServer: {
+			publicPath: '/public/',
+			colors: true,
+			hot: true,
+			inline: true,
+			progress: true,
+			historyApiFallback: true,
+		},
 	}
 	, module: {
 		rules: [
 			{
 				test: /\.tsx?$/,
 				use: 'awesome-typescript-loader',
-				//use: 'ts-loader',
 				exclude: /node_modules/,
 			}
 			, {
@@ -42,21 +47,20 @@ module.exports = {
 				exclude: /node_modules/,
 			}
 			, {
-				test: /\.css$/,
-				loader: ExtractTextPlugin.extract('css-loader?sourceMap'),
-				exclude: /node_modules/,
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: sassLoaders })
 			}
 		]
 	}
 	, resolve: {
-		extensions: ['.webpack.js', '.html', '.ts', '.tsx', '.js', '.jsx', '.css']
+		extensions: ['.webpack.js', '.html', '.ts', '.tsx', '.js', '.jsx', '.scss']
 	}
 	, devtool: 'inline-source-map'
 
 	, plugins: [
 		new CheckerPlugin()
 		, new webpack.HotModuleReplacementPlugin() // enable HMR globally
-    	, new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
+		, new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
 		, new ExtractTextPlugin({ filename: 'bundle.css', disable: false, allChunks: true })
 	]
 };
